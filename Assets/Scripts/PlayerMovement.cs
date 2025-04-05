95a7e5a2dea4abfa7932f211d15762a29c694916
Hamesss <jamesboase123@gmail.com>
2025-04-05 15:14:04 -0400

    add sound manager stuff in main game

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -12,16 +12,34 @@ public class PlayerMovement : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
         {
+            playMoveSound();
             gameObject.transform.position = gameObject.transform.position + new Vector3(0, 1 * speed, 0) * Time.deltaTime;
-        } else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        }
+        else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
+            playMoveSound();
             gameObject.transform.position = gameObject.transform.position + new Vector3(-1 * speed, 0, 0) * Time.deltaTime;
-        } else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        }
+        else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
         {
+            playMoveSound();
             gameObject.transform.position = gameObject.transform.position + new Vector3(0, -1 * speed, 0) * Time.deltaTime;
-        } else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        }
+        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
+            playMoveSound();
             gameObject.transform.position = gameObject.transform.position + new Vector3(1 * speed, 0, 0) * Time.deltaTime;
+        }
+        else {
+            FindFirstObjectByType<AudioManager>().Stop("MovingPlayer");
+        }
+        
+    }
+
+    public void playMoveSound() {
+        if (!FindFirstObjectByType<AudioManager>().isPlaying("MovingPlayer"))
+        {
+            FindFirstObjectByType<AudioManager>().Play("MovingPlayer");
         }
     }
 }
